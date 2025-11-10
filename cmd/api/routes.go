81c0c505680feb664dfd57362795d25b6c4ef63d
Enrisen Tzib <enrisentzib@gmail.com>
2025-11-10 15:23:07 -0600

    Implemented error handling 404, 405, and 500

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,6 +8,14 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFoundResponse(w, r)
+	})
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.methodNotAllowedResponse(w, r)
+	})
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	return router
 }
