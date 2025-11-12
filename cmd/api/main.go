00fd095599c55b4a1df7a366ce66b820456c32c2
Enrisen Tzib <enrisentzib@gmail.com>
2025-11-12 14:15:19 -0600

    connection to database success

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -11,6 +18,9 @@ const version = "1.0.0"
 type configuration struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -19,30 +29,57 @@ type application struct {
 }
 
 func main() {
-	cfg := loadConfig()
-	logger := setupLogger()
+	var cfg configuration
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://animeverse:verse1@localhost/animeverse?sslmode=disable", "PostgreSQL DSN")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
 
-	err := app.serve()
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err = srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
-func loadConfig() configuration {
-	var cfg configuration
-	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
-	flag.Parse()
-	return cfg
-}
+func openDB(cfg configuration) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
 
-func setupLogger() *slog.Logger {
-	var logger *slog.Logger
-	logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
-	return logger
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Syha-01/animeVerseAPI
 go 1.25.0
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
 )
